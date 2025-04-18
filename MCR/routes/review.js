220dd0c691c625240ce9e8d5ac41daa252b12e08
Gianmarco G <giangarof98@gmail.com>
2025-04-18 19:47:48 -0400

    refactor

--- MCR/routes/review.js
+++ MCR/routes/review.js
@@ -6,18 +6,13 @@ import {
     Admin_Or_Owner_Review,
  } from '../../middleware/admin.js';
 import asyncHandler from '../../middleware/asyncHandler.js';
-import { 
-    create, 
-    update,
-    deleteReview,
-    like
 
-} from '../controllers/review.js';
+import { ReviewController } from '../controllers/review.js';
+const review = new ReviewController()
 
-
-router.post('/create/:id', protect, asyncHandler(create))
-router.post('/like/:reviewId', protect, asyncHandler(like))
-router.put('/:reviewId', protect, asyncHandler(update))
-router.delete('/delete/:reviewId', protect, Admin_Or_Owner_Review, asyncHandler(deleteReview))
+router.post('/create/:id', protect, asyncHandler(review.create))
+router.post('/like/:reviewId', protect, asyncHandler(review.like))
+router.put('/:reviewId', protect, asyncHandler(review.update))
+router.delete('/delete/:reviewId', protect, Admin_Or_Owner_Review, asyncHandler(review.deleteReview))
 
 export default router;
